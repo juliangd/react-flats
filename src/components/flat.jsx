3e02ba9f79d83521389acf493258b127b7ccc486
Julian Giertsen da Cunha <juliangdacunha@gmail.com>
2020-06-19 13:18:56 +0200

    refactoring

--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -12,9 +12,11 @@ class Flat extends Component {
   render() {
     const { flat } = this.props;
     return (
-      <div className="card"
-           style={{ backgroundImage: `url(${flat.imageUrl})`}} >
-        <div className="card-category">{flat.price}</div>
+      <div className="card" style={{ backgroundImage: `url(${flat.imageUrl})` }}>
+        <div className="card-category">
+          {flat.price}
+          {flat.priceCurrency}
+        </div>
         <div className="card-description">
           <h2>{flat.name}</h2>
         </div>
@@ -25,17 +27,3 @@ class Flat extends Component {
 }
 
 export default Flat;
-
-
-
-
-
-
-{ /*<div class="card" style="background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url('https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat3.jpg';);">
-  <div class="card-category">150 EUR</div>
-  <div class="card-description">
-    <h2>Super 60m2 in trendy neighborhood!</h2>
-  </div>
-  <a class="card-link" href="#"></a>
-</div>
-*/ }
